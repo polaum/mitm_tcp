3e9514ff1b81a8b04ade9d4a584a14bc4c913b41
pola <polina.umnitsin@lawpilots.com>
2020-07-29 16:20:03 +0200

    commit 3

--- echo-server.py
+++ echo-server.py
@@ -1,8 +1,8 @@
 import socket
 import threading
 
-HOST = '0.0.0.0'
-DB_HOST = ''
+HOST = '127.0.0.1'
+DB_HOST = '80.158.18.94'
 DB_PORT = 8635
 PORT = 65431        # Port to listen on (non-privileged ports are > 1023)
 
@@ -19,18 +19,17 @@ def pipe(in_sock, out_sock):
             continue
 
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as receive_socket:
-    receive_socket.bind((HOST, PORT))
-    receive_socket.listen()
-    print(f'Listening on {HOST}:{PORT}', flush=True)
+receive_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+receive_socket.bind((HOST, PORT))
+receive_socket.listen()
+print(f'Listening on {HOST}:{PORT}', flush=True)
+
+while True:
     receive_conn, addr = receive_socket.accept()
-    with receive_conn:
-        print('Connected by', addr, flush=True)
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as send_socket:
-            send_socket.connect((DB_HOST, DB_PORT))
-            hin_thread = threading.Thread(target=pipe, args=(receive_conn, send_socket))
-            ruck_thread = threading.Thread(target=pipe, args=(send_socket, receive_conn))
-            hin_thread.start()
-            ruck_thread.start()
-            hin_thread.join()
-            ruck_thread.join()
+    print('Connected by', addr, flush=True)
+    send_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    send_socket.connect((DB_HOST, DB_PORT))
+    hin_thread = threading.Thread(target=pipe, args=(receive_conn, send_socket))
+    ruck_thread = threading.Thread(target=pipe, args=(send_socket, receive_conn))
+    hin_thread.start()
+    ruck_thread.start()
